2f911d52d42dc2a1eb15f41dec4d710ce5d5d25c
Akira Morales <akira.k.morales@gmail.com>
2018-11-05 13:37:14 -0500

    Add files via upload

---
+++ src/GettysburgAddressMain.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GettysburgAddressMain {
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		Scanner file = new Scanner(new File("GettysburgAddress.txt"));	
+		ArrayList<String> words = new ArrayList<String>();
+	}
+
+}
