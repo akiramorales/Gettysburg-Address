fb619056e6b74c1255fbbc98eceb09ea167ea9c2
Akira Morales <akira.k.morales@gmail.com>
2018-11-08 13:33:46 -0500

    Add files via upload

--- src/GettysburgAddressMain.java
+++ src/GettysburgAddressMain.java
@@ -8,6 +8,34 @@ public class GettysburgAddressMain {
 	{
 		Scanner file = new Scanner(new File("GettysburgAddress.txt"));	
 		ArrayList<String> words = new ArrayList<String>();
+		
+		while(file.hasNext())
+		{
+			words.add(file.next());
+		}
+		
+		String longestWord = "";
+		for(String wrd : words)
+		{
+			if(wrd.length() > (wrd+1).length())
+			{
+				longestWord = wrd;
+			}
+			else
+			{
+				longestWord = words.get(+1);
+			}
+		}
+		System.out.println("The longest word is " + longestWord);
+		int numWords = 0;
+		int totalLength = 0;
+		for(String wrd : words)
+		{
+			numWords ++;
+			totalLength = wrd.length();
+		}
+		int averageLength = totalLength/numWords;
+		System.out.println("The average word length is " + averageLength + " letters long.");
 	}
 
 }
